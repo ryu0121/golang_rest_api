f275ccc57b5dcd14b98907a97bedc7f60e5041ed
ryuhei-kashi <manchester.ryu.121@gmail.com>
2022-02-14 21:39:42 +0900

    fix: 一部修正

--- app/infrastructure/sqlhandler.go
+++ app/infrastructure/sqlhandler.go
@@ -2,9 +2,9 @@ package infrastructure
 
 import (
 	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
 	"golang_rest_api/app/interfaces/database"
 
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type SqlHandler struct {
@@ -12,7 +12,7 @@ type SqlHandler struct {
 }
 
 func NewSqlHandler() *SqlHandler {
-	conn, err := sql.Open("mysql", "r-kashi:soccer121ryu@tcp(127.0.0.1:3306)/golang_rest_api")
+	conn, err := sql.Open("mysql", "r-kashi:password@tcp(127.0.0.1:3306)/golang_rest_api")
 	if err != nil {
 		panic(err.Error)
 	}
@@ -22,45 +22,45 @@ func NewSqlHandler() *SqlHandler {
 }
 
 func (handler *SqlHandler) Exec(statement string, args ...interface{}) (database.Result, error) {
-    res := SqlResult{}
-    result, err := handler.Conn.Exec(statement, args...)
-    if err != nil {
-        return res, err
-    }
-    res.Result = result
-    return res, nil
+	res := SqlResult{}
+	result, err := handler.Conn.Exec(statement, args...)
+	if err != nil {
+		return res, err
+	}
+	res.Result = result
+	return res, nil
 }
 
 func (handler *SqlHandler) Query(statement string, args ...interface{}) (database.Row, error) {
-    rows, err := handler.Conn.Query(statement, args...)
-    if err != nil {
-        return new(SqlRow), err
-    }
-    row := new(SqlRow)
-    row.Rows = rows
-    return row, nil
+	rows, err := handler.Conn.Query(statement, args...)
+	if err != nil {
+		return new(SqlRow), err
+	}
+	row := new(SqlRow)
+	row.Rows = rows
+	return row, nil
 }
 
 type SqlResult struct {
-    Result sql.Result
+	Result sql.Result
 }
 
 func (r SqlResult) LastInsertId() (int64, error) {
-    return r.Result.LastInsertId()
+	return r.Result.LastInsertId()
 }
 
 type SqlRow struct {
-    Rows *sql.Rows
+	Rows *sql.Rows
 }
 
 func (r SqlRow) Scan(dest ...interface{}) error {
-    return r.Rows.Scan(dest...)
+	return r.Rows.Scan(dest...)
 }
 
 func (r SqlRow) Next() bool {
-    return r.Rows.Next()
+	return r.Rows.Next()
 }
 
 func (r SqlRow) Close() error {
-    return r.Rows.Close()
+	return r.Rows.Close()
 }
